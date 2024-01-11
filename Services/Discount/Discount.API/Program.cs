1116e0fc85cd17734ac835d73240ce5d86a09282
Mahdi Shaban <mahdishaban33@gmail.com>
2024-01-11 10:18:48 +0330

    Discount Controller

--- Services/Discount/Discount.API/Program.cs
+++ Services/Discount/Discount.API/Program.cs
@@ -1,6 +1,9 @@
+using Discount.API.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
