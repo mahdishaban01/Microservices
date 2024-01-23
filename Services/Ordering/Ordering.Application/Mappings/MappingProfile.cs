b701c09dfbf1ca9f3b83036a454935eaf8d97df6
Mahdi Shaban <mahdishaban33@gmail.com>
2024-01-23 23:29:48 +0330

    Mappings

--- Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
+++ Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
@@ -1,9 +1,14 @@
 ﻿using AutoMapper;
+using Ordering.Application.Features.Orders.Queries;
+using Ordering.Domain.Entities;
 
 namespace Ordering.Application.Mappings
 {
     public class MappingProfile : Profile
     {
-
+        public MappingProfile()
+        {
+            CreateMap<Order, OrdersVm>().ReverseMap();
+        }
     }
 }
